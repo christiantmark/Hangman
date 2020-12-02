7cba4d62796589799c05f15ee91fee690c12aec5
Christian Mark <christiantmark@gmail.com>
2020-12-01 22:40:11 -0500

    updating hangman with wordlist and a main function to allow players to play against the computer

--- hangman.py
+++ hangman.py
@@ -5,3 +5,10 @@ from words import word_list
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
+def play(word):
+    word_completion = "_" + len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
