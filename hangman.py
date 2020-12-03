5e6d9b53bd83467ad871a59ed5c57becca9235fb
Christian Mark <christiantmark@gmail.com>
2020-12-02 23:28:23 -0500

    provided error messaging for wrong answer

--- hangman.py
+++ hangman.py
@@ -13,3 +13,96 @@ def play(word):
     guessed_words = []
     tries = 6
     print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print('\n')
+    while not guessed and tries > 0:
+        guess = input("Please guess a word or letter: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+
+        elif len(guess) ==len(word) and guess.isalpha():
+
+        else:
+           print("The guess is not valid. PLease try again")   
+        print(display_hangman)
+        print(word_completion)   
+
+
+
+
+
+
+
+    def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
